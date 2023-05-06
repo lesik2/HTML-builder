7087fb15db4a7ec2fce15f47c0d9bd24ffde3842
Aliaksei Pukhalski <lesikpuhalskij@gmail.com>
2023-05-06 21:20:53 +0300

    init: resolve first task.

---
+++ 01-read-file/index.js
@@ -0,0 +1,10 @@
+
+const fs = require('fs');
+const path=require('path');
+
+
+const readStream = fs.createReadStream(path.join(__dirname,'text.txt'),'utf-8');
+let data = '';
+readStream.on('data',chunk=>data += chunk);
+readStream.on('error',error=>console.log(error.message));
+readStream.on('end',() => console.log(data));
